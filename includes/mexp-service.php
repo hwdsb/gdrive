75a54e8808a1260f1037e2535869016a08531f6a
r-a-y <r-a-y@gmx.com>
2018-11-20 20:30:08 +0900

    Bump JS version to purge browser cache.
    See #21.

--- includes/mexp-service.php
+++ includes/mexp-service.php
@@ -116,7 +116,7 @@ class MEXP_GDrive_Service extends MEXP_Service {
 			'mexp-service-gdrive',
 			MEXP_GDrive::$URL . '/assets/js.js',
 			array( 'jquery', 'mexp' ),
-			'20170626'
+			'20181120'
 		);
 
 		wp_enqueue_style(
