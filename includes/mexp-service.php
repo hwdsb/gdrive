f0b88a2c80c9970312111bb9e4737222c903f7ca
r-a-y <r-a-y@gmx.com>
2020-04-09 16:31:52 -0700

    Gutenberg: Fix stylistic issues for WordPress 5.4.0.
    See #28.

--- includes/mexp-service.php
+++ includes/mexp-service.php
@@ -93,7 +93,7 @@ class MEXP_GDrive_Service extends MEXP_Service {
 			wp_enqueue_style(
 				'mexp-gdrive-gutenberg',
 				MEXP_GDrive::$URL . '/assets/block.css',
-				'20190901'
+				'20200409'
 			);
 
 			// i18n. Developer tools haven't stabilized yet...
